79600c2316019de47292f98df61e2a3e315462f3
Adoel Rachman <adoel.cs@gmail.com>
2020-07-10 08:17:09 +0700

    build

---
+++ build/precache-manifest.4d82d6b4554e2c0daf3f64e7b189043d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef840f486eb594fcc6da05ab1aa77e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e31874b8b7eb08fb8223",
+    "url": "/static/css/2.81489842.chunk.css"
+  },
+  {
+    "revision": "d8c1c09d28e846cee1f1",
+    "url": "/static/css/main.a74ce3ae.chunk.css"
+  },
+  {
+    "revision": "e31874b8b7eb08fb8223",
+    "url": "/static/js/2.c0fb7195.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c0fb7195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c1c09d28e846cee1f1",
+    "url": "/static/js/main.f903f9a5.chunk.js"
+  },
+  {
+    "revision": "58e95fa7832e2f8e65a4",
+    "url": "/static/js/runtime-main.aedb1fe1.js"
+  },
+  {
+    "revision": "0c0cbbd6f789c11109da41723cc5f2a9",
+    "url": "/static/media/1.0c0cbbd6.jpg"
+  },
+  {
+    "revision": "31651a55294e31c2192b5f7336dd8bed",
+    "url": "/static/media/2.31651a55.jpg"
+  },
+  {
+    "revision": "9bfbb5a797007ca75475590e1389c5cb",
+    "url": "/static/media/3.9bfbb5a7.jpg"
+  },
+  {
+    "revision": "3674a9dee5fd66eadae9bf05e78281a2",
+    "url": "/static/media/4.3674a9de.jpg"
+  },
+  {
+    "revision": "bca1fc4ba8b9e2ec5a333671390aeba1",
+    "url": "/static/media/5.bca1fc4b.jpg"
+  },
+  {
+    "revision": "fc8745e7dc3bed1a461f3dc4011b914c",
+    "url": "/static/media/6.fc8745e7.jpg"
+  },
+  {
+    "revision": "f11f440bc405560b25d7efff5bfbd6a6",
+    "url": "/static/media/backgorund.f11f440b.jpg"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  }
+]);
